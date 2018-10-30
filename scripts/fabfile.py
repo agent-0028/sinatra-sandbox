e07da362d406afb8c189b704ff7465bfe2b861c6
“Tinisi” <jgrosz@tinisi.com>
2018-10-29 21:21:01 -0500

    Add no-op deploy job and workflow

---
+++ scripts/fabfile.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+from fabric.api import *
+from fabric.contrib import *
+
+@task
+def test_sudo_connection():
+    sudo('ls')
+
+@task
+def test_connection():
+    sleep(30)
+    run('ls')
+
+@task
+def docker_compose_deploy(image_tag='latest', file_path='', project_name=''):
+
+    target_file_path = '~/deploy_temp/docker-compose-deploy.yml'
+    put(file_path, target_file_path)
+
+    sleep(30)
+
+    with shell_env(DEPLOY_TAG=image_tag):
+        run('source /home/tinisi/bin/set-docker-host-env.sh && \
+            docker-compose \
+            --file {target_file_path} \
+            --project-name  {project_name} \
+            stop'.format(target_file_path=target_file_path, project_name=project_name))
+
+        run('source /home/tinisi/bin/set-docker-host-env.sh && \
+            docker-compose \
+            --file {target_file_path} \
+            --project-name {project_name} \
+            up \
+            --detach'.format(target_file_path=target_file_path, project_name=project_name))
+
